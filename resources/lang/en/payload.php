d578c1ecfb51a7b22b9cd824812147aa060b8110
leandroaroca <41336236+leandroaroca@users.noreply.github.com>
2021-05-13 17:36:36 -0400

    Add exceptions for hex and eval functions

---
+++ resources/lang/en/payload.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'store_success' => 'Data stored successfully',
+    'store_fail' => 'Error storing data',
+];
---
+++ resources/lang/es/payload.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'store_success' => 'Datos almacenados con éxito',
+    'store_fail' => 'Error al almacenar los datos',
+];
